06b1a474c0ba8f25d972e1ab7706ffdbe9564480
RubyDeveloper1 <ruby_developer1@yahoo.com>
2020-01-10 19:29:52 +0300

    Add test for car

--- car.rb
+++ car.rb
@@ -15,6 +15,6 @@ class Car
   end
 end
 
-car = Car.new
-car.add_fuel 10
-puts "Range is #{car.range}"
+#car = Car.new
+#car.add_fuel 10
+#puts "Range is #{car.range}"
---
+++ car_spec.rb
@@ -0,0 +1,9 @@
+require './car'
+
+describe Car do
+  it "must return range" do
+    car = Car.new          # arrange
+    car.add_fuel 10        # act
+    expect(car.range).to eq 200
+  end
+end 
